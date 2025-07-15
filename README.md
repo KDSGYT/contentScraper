b36409832289e18b9c0ccb590832a26802ab5637
Karan Pal Singh <kdsgyt@gmail.com>
2025-07-15 19:23:09 -0400

    Having issues with login.

--- README.md
+++ README.md
@@ -10,7 +10,16 @@ contentScraper/
 ├── config.py
 └── main.py
 
+# Function Workflow
+1. Scrape new posts/videos.
+2. Filter by your criteria.
+3. Analyze video metrics for significant changes.
+4. Compose and send an email report.
+
 # Video Criteria
 1. Views >= 5xFollowers
 2. Viewer Retention>= 50%
 3. Video length >= 15 seconds
+
+# Todo
+1. Install selected package
---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+# It serves as a basic example of how to run a Python script.
+# To run this script, use the command: python3 main.py
+# Ensure you have Python 3 installed on your system.
+# You can modify this script to perform other tasks as needed.
+
+# For example, you can add more print statements or define functions.
+
+import instaloader as ig
+import os
+import webbrowser
+USERNAME = os.getenv('INSTA_USERNAME')
+PASSWORD = os.getenv('INSTA_PASSWORD')
+
+print(f"Logging in as {USERNAME}")
+
+L = ig.Instaloader(download_videos=True, download_pictures=False)
+
+# login to Instagram
+try:
+    L.login(USERNAME, PASSWORD)
+    # L.interactive_login(USERNAME)
+    print("Login successful!")
+#     #use except to print ehrror message if login fails
+
+
+# implementing the login having issue with authentication: password was entered correctly however login with browser is reqired. Stopping coding for now since there is redundant error showing wrong password. 
+
+except Exception as e:
+    print("Login failed. Login with Browser.")
+    # Extract the URL from the error message if present
+    error_str = str(e)
+    if "https://" in error_str:
+        url = error_str.split("to ")[-1].split(" -")[0].strip()
+        print(f"Opening browser for checkpoint: {url}")
+        webbrowser.open(url)
+    print(e)
+
+# L.test_login()
+
+# hashtag = "funny"  # Replace with your desired hashtag
+# for post in ig.Hashtag.from_name(L.context, hashtag).get_posts():
+#     # print a list of posts with the specified hashtag 
+#     print(f"Post: {post.shortcode}, Likes: {post.likes}, Comments: {post.comments}, Video: {post.is_video}")
+#     if post.is_video:
+#         L.download_post(post, target=f"#{hashtag}")
+
+        
